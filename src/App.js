5e35e33085a362f496a74dfa4954c69d24e11226
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2022-10-15 02:13:13 -0400

    fix: fix variable and font errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import "./scss/app.scss";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <h1 style={{ fontSize: "60px" }}>Header</h1>
     </div>
   );
 }
