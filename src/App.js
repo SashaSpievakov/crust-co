cfa57211c09e69e147163cea01b35977570a2beb
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2022-10-16 01:49:35 -0400

    feat: add basic routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Route, Routes } from "react-router-dom";
+
 import "./scss/app.scss";
 import Header from "./components/Header";
 import Home from "./pages/Home";
@@ -9,7 +11,10 @@ function App() {
       <Header />
       <div className="content">
         <div className="container">
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
