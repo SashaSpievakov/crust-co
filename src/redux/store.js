7ba936d310db69bccc4e6a2d09f991dfbc717793
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2022-10-18 20:46:11 -0400

    add cart slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import categorySlice from "./slices/categorySlice";
 import sortSlice from "./slices/sortSlice";
 import searchSlice from "./slices/searchSlice";
+import cart from "./slices/cartSlice";
 
 export const store = configureStore({
   reducer: {
     activeCategory: categorySlice,
     activeSort: sortSlice,
     searchValue: searchSlice,
+    cart,
   },
 });
