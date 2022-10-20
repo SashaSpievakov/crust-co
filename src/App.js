8696610218b11a2ccbc2a38566c513624bd78750
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2022-10-20 03:28:45 -0400

    feat: add Outlet

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 
 import "./scss/app.scss";
-import Header from "./components/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Cart from "./pages/Cart";
 import FullItem from "./pages/FullItem";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/item/:id" element={<FullItem />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="item/:id" element={<FullItem />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
