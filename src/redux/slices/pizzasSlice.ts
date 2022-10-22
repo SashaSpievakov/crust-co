eca5869729584e7633648d262913d61523456a41
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2022-10-22 01:12:26 -0400

    refactor: add types for everething in pizza slice

--- src/redux/slices/pizzasSlice.ts
+++ src/redux/slices/pizzasSlice.ts
@@ -1,24 +1,6 @@
 import axios from "axios";
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../store";
-import { ICartItem } from "./cartSlice";
-
-interface FetchItems {
-  activeCategory: number,
-  sortedPropertyName: string,
-}
-
-export const fetchItems = createAsyncThunk<ICartItem[], FetchItems>(
-  "pizzas/fetchItemsStatus",
-  async ({ activeCategory, sortedPropertyName }) => {
-    const { data } = await axios.get<ICartItem[]>(
-      `https://6344adb1dcae733e8fe3067a.mockapi.io/pizza-items?${
-        activeCategory > 0 ? `category=${activeCategory}&` : ""
-      }sortBy=${sortedPropertyName}`
-    );
-    return data;
-  }
-);
 
 export interface PizzaItem {
   category: number,
@@ -30,6 +12,23 @@ export interface PizzaItem {
   sizes: number[],
   types: number[],
 }
+
+interface FetchItems {
+  activeCategory: number,
+  sortedPropertyName: string,
+}
+
+export const fetchItems = createAsyncThunk<PizzaItem[], FetchItems>(
+  "pizzas/fetchItemsStatus",
+  async ({ activeCategory, sortedPropertyName }) => {
+    const { data } = await axios.get<PizzaItem[]>(
+      `https://6344adb1dcae733e8fe3067a.mockapi.io/pizza-items?${
+        activeCategory > 0 ? `category=${activeCategory}&` : ""
+      }sortBy=${sortedPropertyName}`
+    );
+    return data;
+  }
+);
 
 interface PizzasSliceState {
   status: "loading" | "success" | "rejected",
@@ -45,24 +44,26 @@ export const pizzasSlice = createSlice({
   name: "pizzas",
   initialState,
   reducers: {
-    setItems: (state, action) => {
+    setItems: (state, action: PayloadAction<PizzaItem[]>) => {
       state.items = action.payload;
     },
   },
-  extraReducers: {
-    [fetchItems.pending]: state => {
+  extraReducers: (builder) => {
+    builder.addCase(fetchItems.pending, (state) => {
       state.items = [];
       state.status = "loading";
-    },
-    [fetchItems.fulfilled]: (state, action) => {
+    });
+
+    builder.addCase(fetchItems.fulfilled, (state, action) => {
       state.items = action.payload;
       state.status = "success";
-    },
-    [fetchItems.rejected]: state => {
+    });
+
+    builder.addCase(fetchItems.rejected, (state) => {
       state.items = [];
       state.status = "rejected";
-    },
-  },
+    });
+  }
 });
 
 export const selectPizzasData = (state: RootState) => state.pizzas;
