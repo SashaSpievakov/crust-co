f378063f5e4fb778bbf0b8db07cccbbfeb6d511a
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2022-10-28 02:25:52 -0400

    feat: install ahooks

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useWhyDidYouUpdate } from 'ahooks';
 
 import { selectCategory, setCategory } from '../redux/slices/categorySlice';
 
