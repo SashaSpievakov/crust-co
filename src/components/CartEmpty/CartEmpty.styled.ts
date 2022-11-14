df48be45b41b37a184a927d16fdd77e301f4489b
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2022-11-14 04:52:23 -0500

    feat: set EmptyCart for small screens

--- src/components/CartEmpty/CartEmpty.styled.ts
+++ src/components/CartEmpty/CartEmpty.styled.ts
@@ -2,13 +2,17 @@ import styled from "styled-components";
 
 export const Article = styled.article`
   margin: 80px auto;
-  width: 600px;
+  max-width: 600px;
   text-align: center;
 
   h2 {
     font-size: 36px;
     font-weight: 700;
     margin-bottom: 25px;
+
+    @media screen and (max-width: 500px) {
+      font-size: 30px;
+    }
   }
 
   p {
@@ -16,11 +20,19 @@ export const Article = styled.article`
     line-height: 145.4%;
     letter-spacing: 0.01em;
     color: #777777;
+
+    @media screen and (max-width: 500px) {
+      font-size: 15px;
+    }
   }
 
   img {
     display: block;
     width: 300px;
     margin: 45px auto 60px;
+
+    @media screen and (max-width: 500px) {
+      width: 200px;
+    }
   }
 `
