1f6ac627fb00bbb3f6d8b2fe7121d01c2b55a6c2
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-01-05 18:47:10 -0500

    refactor: add memoization for Item Cart component

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import { BsDashLg, BsPlusLg } from "react-icons/bs";
 
 import { addItem, removeItem, removeItems } from "../../store/slices/cartSlice";
@@ -23,79 +24,81 @@ interface CartItemProps {
   count: number;
 }
 
-const CartItem = ({ id, name, price, size, type, count }: CartItemProps) => {
-  const dispatch = useAppDispatch();
+const CartItem = memo(
+  ({ id, name, price, size, type, count }: CartItemProps) => {
+    const dispatch = useAppDispatch();
 
-  const onClickAdd = () => {
-    const item: ICartItem = {
-      id,
-      name,
-      price,
-      size,
-      type,
-      count,
+    const onClickAdd = () => {
+      const item: ICartItem = {
+        id,
+        name,
+        price,
+        size,
+        type,
+        count,
+      };
+      dispatch(addItem(item));
     };
-    dispatch(addItem(item));
-  };
 
-  const onClickRemove = () => {
-    const item: ICartItem = {
-      id,
-      name,
-      price,
-      size,
-      type,
-      count,
+    const onClickRemove = () => {
+      const item: ICartItem = {
+        id,
+        name,
+        price,
+        size,
+        type,
+        count,
+      };
+      dispatch(removeItem(item));
     };
-    dispatch(removeItem(item));
-  };
 
-  const onClickDelete = () => {
-    const item: ICartItem = {
-      id,
-      name,
-      price,
-      size,
-      type,
-      count,
+    const onClickDelete = () => {
+      const item: ICartItem = {
+        id,
+        name,
+        price,
+        size,
+        type,
+        count,
+      };
+      dispatch(removeItems(item));
     };
-    dispatch(removeItems(item));
-  };
 
-  return (
-    <Article>
-      <ImgWrapper>
-        <img
-          src={`./assets/img/pizza${id}.png`}
-          alt="Pizza"
-          width="80"
-          height="80"
-        />
-      </ImgWrapper>
-      <InfoWrapper>
-        <h3>{name}</h3>
-        <p>
-          {type} dough, {size} inch
-        </p>
-      </InfoWrapper>
-      <Counter>
-        <ButtonCircle disabled={count === 1} onClick={onClickRemove}>
-          <CountIcon as={BsDashLg} />
-        </ButtonCircle>
-        <b>{count}</b>
-        <ButtonCircle onClick={onClickAdd}>
-          <CountIcon as={BsPlusLg} />
-        </ButtonCircle>
-      </Counter>
-      <Price>
-        <b>{price * count}$</b>
-      </Price>
-      <Remove>
-        <ButtonCircle onClick={onClickDelete} remove>
-          <CountIcon as={BsPlusLg} />
-        </ButtonCircle>
-      </Remove>
-    </Article>
-  );
-};
+    return (
+      <Article>
+        <ImgWrapper>
+          <img
+            src={`./assets/img/pizza${id}.png`}
+            alt="Pizza"
+            width="80"
+            height="80"
+          />
+        </ImgWrapper>
+        <InfoWrapper>
+          <h3>{name}</h3>
+          <p>
+            {type} dough, {size} inch
+          </p>
+        </InfoWrapper>
+        <Counter>
+          <ButtonCircle disabled={count === 1} onClick={onClickRemove}>
+            <CountIcon as={BsDashLg} />
+          </ButtonCircle>
+          <b>{count}</b>
+          <ButtonCircle onClick={onClickAdd}>
+            <CountIcon as={BsPlusLg} />
+          </ButtonCircle>
+        </Counter>
+        <Price>
+          <b>{price * count}$</b>
+        </Price>
+        <Remove>
+          <ButtonCircle onClick={onClickDelete} remove>
+            <CountIcon as={BsPlusLg} />
+          </ButtonCircle>
+        </Remove>
+      </Article>
+    );
+  },
+);
 export default CartItem;
