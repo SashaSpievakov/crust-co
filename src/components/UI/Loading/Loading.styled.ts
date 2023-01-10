2f2b7343c11739447f7ae9aa0726567a3a033385
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-01-10 17:32:11 -0500

    refactor: add adaptivity for Loading component

--- src/components/UI/Loading/Loading.styled.ts
+++ src/components/UI/Loading/Loading.styled.ts
@@ -3,6 +3,10 @@ import styled from "styled-components";
 const Loading = styled.div`
   font-size: 45px;
   color: ${({ theme }) => theme.colors.primary};
+
+  @media screen and (max-width: 400px) {
+    font-size: 35px;
+  }
 `;
 
 export default Loading;
