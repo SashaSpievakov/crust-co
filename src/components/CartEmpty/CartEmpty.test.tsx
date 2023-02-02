0e6f531a14d0cf2717d49284970be5af739dd255
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-02-02 00:52:00 -0500

    refactor: setup test helpers

--- src/components/CartEmpty/CartEmpty.test.tsx
+++ src/components/CartEmpty/CartEmpty.test.tsx
@@ -1,38 +1,13 @@
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { ThemeProvider } from 'styled-components';
-import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { screen } from '@testing-library/react';
 
 import CartEmpty from './CartEmpty';
-import { darkTheme } from '../../styles/Themes.styled';
-import MainLayout from '../../layouts/MainLayout';
-import Cart from '../../pages/Cart/Cart';
-import FullItem from '../../pages/FullItem/FullItem';
-import NotFound from '../../pages/NotFound/NotFound';
-import Home from '../../pages/Home/Home';
-import { store } from '../../store/store';
+import renderWithProviders from '../../tests/helpers/renderWithRouter';
 
 describe('Cart Empty Test', () => {
   beforeEach(() => {
-    render(
-      <Provider store={store}>
-        <ThemeProvider theme={darkTheme}>
-          <MemoryRouter>
-            <Routes>
-              <Route path="/" element={<MainLayout />}>
-                <Route index element={<Home />} />
-                <Route path="cart" element={<Cart />} />
-                <Route path="item/:id" element={<FullItem />} />
-                <Route path="*" element={<NotFound />} />
-              </Route>
-            </Routes>
-            <CartEmpty />
-          </MemoryRouter>
-        </ThemeProvider>
-      </Provider>,
-    );
+    renderWithProviders(<CartEmpty />);
   });
 
   test('renders heading', () => {
