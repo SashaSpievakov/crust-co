cb02467afe1390850b922a8d948ebb78e22d25c7
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-02-02 19:16:30 -0500

    feat: download and implement snapshots for the Footer

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,41 +1,12 @@
 import '@testing-library/jest-dom';
-import { screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 
 import Footer from './Footer';
 import renderWithTheme from '../../tests/helpers/renderWithTheme';
 
-describe('Cart Empty Test', () => {
-  beforeEach(() => {
-    renderWithTheme(<Footer />);
+describe('Footer Test', () => {
+  test('renders the Footer component', () => {
+    const snapshot = renderer.create(renderWithTheme(<Footer />)).toJSON();
+    expect(snapshot).toMatchSnapshot();
   });
-
-  test('renders email link', () => {
-    const link = screen.getByRole('link', {
-      name: /spievakov@gmail\.com/i,
-    });
-    expect(link).toBeInTheDocument();
-  });
-
-  test('renders phone link', () => {
-    const link = screen.getByRole('link', {
-      name: /\+1 \(647\) 473-9502/i,
-    });
-    expect(link).toBeInTheDocument();
-  });
-
-  test('renders location link', () => {
-    const link = screen.getByRole('link', {
-      name: /88 alice st/i,
-    });
-    expect(link).toBeInTheDocument();
-  });
-
-  test('renders copyrights', () => {
-    const copyright = screen.getByText(/© 2022 oleksandr spievakov/i);
-    expect(copyright).toBeInTheDocument();
-  });
-
-  // test('renders snapshot', () => {
-  //   const snapshot = renderer;
-  // });
 });
--- src/tests/helpers/renderWithTheme.tsx
+++ src/tests/helpers/renderWithTheme.tsx
@@ -2,14 +2,11 @@
 
 import { ReactElement } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { render } from '@testing-library/react';
 
 import { lightTheme } from '../../styles/Themes.styled';
 
 const renderWithTheme = (compenent: ReactElement) => {
-  return {
-    ...render(<ThemeProvider theme={lightTheme}>{compenent}</ThemeProvider>),
-  };
+  return <ThemeProvider theme={lightTheme}>{compenent}</ThemeProvider>;
 };
 
 export default renderWithTheme;
