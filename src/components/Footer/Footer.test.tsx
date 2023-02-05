58f831c1bba9bb878c44874e4539aa4635a5aab0
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-02-05 06:28:47 -0500

    refactor: refactor Footer tests

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,11 +1,11 @@
 import '@testing-library/jest-dom';
 
-// import Footer from './Footer';
-// import rendererWithTheme from '../../tests/helpers/renderer/rendererWithTheme';
+import Footer from './Footer';
+import rendererWithProviders from '../../tests/helpers/rendererWithProviders';
 
 describe('Footer Test', () => {
-  // test('renders the Footer component', () => {
-  //   const snapshot = rendererWithTheme(<Footer />);
-  //   expect(snapshot).toMatchSnapshot();
-  // });
+  test('renders the Footer component', () => {
+    const snapshot = rendererWithProviders(<Footer />);
+    expect(snapshot).toMatchSnapshot();
+  });
 });
