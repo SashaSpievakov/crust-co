eda04f6b3b6a24d3521592618e8ddf46964cfd1b
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-02-07 01:35:35 -0500

    feat: add different responses to the pizzaItems mock request

--- src/tests/mocks/api/handlers.ts
+++ src/tests/mocks/api/handlers.ts
@@ -10,8 +10,15 @@ const handlers = [
   ),
   rest.get(
     'https://6344adb1dcae733e8fe3067a.mockapi.io/pizza-items',
-    (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json(mockItems), ctx.delay(20));
+    async (req, res, ctx) => {
+      const category = req.url.searchParams.get('category');
+      if (category) {
+        if (req.url.searchParams.get('category') === '1') {
+          return res(ctx.json([mockItems[1]]), ctx.status(200), ctx.delay(20));
+        }
+        return res(ctx.json(mockItems), ctx.status(200), ctx.delay(20));
+      }
+      return res(ctx.json(mockItems), ctx.status(200), ctx.delay(20));
     },
   ),
 ];
