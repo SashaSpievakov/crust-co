16868e10f1587235f581386ce81a51a2bedb918e
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-02-08 03:52:13 -0500

    fix: add adaptivity for NotFound page

--- src/pages/NotFound/NotFound.styled.ts
+++ src/pages/NotFound/NotFound.styled.ts
@@ -4,11 +4,35 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-bottom: 75px;
+
+  @media screen and (max-width: 900px) {
+    margin-bottom: 50px;
+  }
+
+  @media screen and (max-width: 650px) {
+    margin-bottom: 20px;
+  }
 `;
 
 const Title = styled.h1`
-  padding: 150px;
   font-size: 64px;
+  text-align: center;
+  padding: 150px 0;
+
+  @media screen and (max-width: 900px) {
+    font-size: 50px;
+    padding: 100px 0;
+  }
+
+  @media screen and (max-width: 650px) {
+    font-size: 35px;
+    padding: 50px 0;
+  }
+
+  @media screen and (max-width: 500px) {
+    padding: 40px 0;
+  }
 `;
 
 export { Title, Wrapper };
