7968fbd3ba52ec012b853dd3dc567272055d5e8f
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-02-08 07:46:34 -0500

    refactor: refactor ProductConteiner tests with importing mock data

--- src/components/ProductsContainer/ProductContainer.test.tsx
+++ src/components/ProductsContainer/ProductContainer.test.tsx
@@ -4,56 +4,18 @@ import { screen } from '@testing-library/react';
 import ProductsContainer from './ProductsContainer';
 import rendererWithAllProviders from '../../tests/helpers/rendererWithProviders';
 import renderWithProviders from '../../tests/helpers/renderWithProviders';
-import { IPizzaItem } from '../../models/IPizzaItem';
-
-const ProductsContainerItems: IPizzaItem[] = [
-  {
-    id: '12',
-    name: 'Vegetarian Pizza',
-    description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid nisi aspernatur debitis quod suscipit, ',
-    types: [0, 1],
-    sizes: [12, 14, 16],
-    price: 12,
-    category: 2,
-    rating: 8,
-  },
-  {
-    id: '9',
-    name: 'Meat Pizza',
-    description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus, iste.',
-    types: [0],
-    sizes: [12, 16],
-    price: 14,
-    category: 1,
-    rating: 15,
-  },
-  {
-    id: '2',
-    name: 'Pepperoni Pizza',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, cum recusandae! Quidem, quaerat',
-    types: [0],
-    sizes: [12, 14],
-    price: 10,
-    category: 4,
-    rating: 5,
-  },
-];
+import { mockItems } from '../../tests/mocks/api/mockData';
 
 describe('ProductsContainer Tests', () => {
   test('renders the ProductsContainer component', () => {
     const snapshot = rendererWithAllProviders(
-      <ProductsContainer isLoading={false} items={ProductsContainerItems} />,
+      <ProductsContainer isLoading={false} items={mockItems} />,
     );
     expect(snapshot).toMatchSnapshot();
   });
 
   test('renders the ProductsContainer component skeletons', () => {
-    renderWithProviders(
-      <ProductsContainer isLoading items={ProductsContainerItems} />,
-    );
+    renderWithProviders(<ProductsContainer isLoading items={mockItems} />);
     const skeletons = screen.getAllByRole('presentation');
 
     expect(skeletons[0]).toBeInTheDocument();
@@ -62,13 +24,13 @@ describe('ProductsContainer Tests', () => {
 
   test('renders components with a search value', () => {
     renderWithProviders(
-      <ProductsContainer isLoading={false} items={ProductsContainerItems} />,
+      <ProductsContainer isLoading={false} items={mockItems} />,
       false,
       '/',
-      { searchValue: { value: '  Meat Pizza ' } },
+      { searchValue: { value: '  Srimp Pizza ' } },
     );
     const heading = screen.getByRole('heading');
 
-    expect(heading).toHaveTextContent(/meat pizza/i);
+    expect(heading).toHaveTextContent(/srimp pizza/i);
   });
 });
--- src/tests/mocks/api/mockData.ts
+++ src/tests/mocks/api/mockData.ts
@@ -5,7 +5,7 @@ export const mockItems: IPizzaItem[] = [
     id: '3',
     name: 'Srimp Pizza',
     description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid nisi aspernatur debitis quod suscipit, vitae placeat natus ipsum inventore esse rerum animi facere numquam saepe vero mollitia quibusdam at voluptates est commodi laudantium',
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam vel eos cum ullam sed, officiis cupiditate autem. Illo, ipsam dolorem! Minus qui sequi reiciendis ipsa dolor eligendi laudantium nihil iusto libero illum commodi esse nulla velit, corporis sint. Illo laborum optio nostrum qui. Laboriosam ipsum ullam, velit itaque iure officia',
     types: [0, 1],
     sizes: [12, 14, 16],
     price: 10,
@@ -16,12 +16,23 @@ export const mockItems: IPizzaItem[] = [
     id: '2',
     name: 'Pepperoni Pizza',
     description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid nisi aspernatur debitis quod suscipit, vitae placeat natus ipsum inventore esse rerum animi facere numquam saepe vero mollitia quibusdam at voluptates est commodi laudantium',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aliquid porro sequi ducimus, vel consequuntur modi, eaque quo ratione a assumenda numquam, aperiam odit illo. Impedit, dolores quae mollitia saepe ad ipsa temporibus in pariatur corporis facere ipsam repellendus iste neque quis repudiandae sequi aliquid ab dolorem consectetur vero, dicta commodi',
     types: [0],
     sizes: [12, 14],
     price: 12,
     category: 1,
     rating: 13,
+  },
+  {
+    id: '6',
+    name: 'Diablo',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aliquid porro sequi ducimus, vel consequuntur modi, eaque quo ratione a assumenda numquam, aperiam odit illo. Impedit, dolores quae mollitia saepe ad ipsa temporibus in pariatur corporis facere ipsam repellendus iste neque quis repudiandae sequi aliquid ab dolorem consectetur vero, dicta commodi',
+    types: [0, 1],
+    sizes: [12, 16],
+    price: 15,
+    category: 4,
+    rating: 16,
   },
 ];
 
