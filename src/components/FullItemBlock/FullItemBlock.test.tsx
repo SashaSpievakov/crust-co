d771ea3c47d71102113ca63b69ca3ae7a1e90600
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-02-10 05:07:24 -0500

    refactor: add two additional tests for FullItemBlock integrations

--- src/components/FullItemBlock/FullItemBlock.test.tsx
+++ src/components/FullItemBlock/FullItemBlock.test.tsx
@@ -74,15 +74,62 @@ describe('FullItemBlock Tests', () => {
       renderWithProviders(<FullItemBlock item={mockItem} />);
     });
 
-    test('checks size click', () => {
+    test('checks disabled minus button after type switching', () => {
       const sizeItem = screen.getByText(/16 inch/i);
-      const price = screen.getByRole('heading', {
-        level: 3,
-      });
+      const buttonAdd = screen.getByText(/add/i);
+      userEvent.click(buttonAdd);
+
+      const buttonPlus = screen.getByTestId('itemsHandlerPlus');
+      const buttonMinus = screen.getByTestId('itemsHandlerMinus');
+      const count = screen.getByText('1');
+
+      userEvent.click(buttonPlus);
+      userEvent.click(buttonPlus);
 
       userEvent.click(sizeItem);
+      userEvent.click(buttonPlus);
+      userEvent.click(buttonMinus);
+      userEvent.click(buttonMinus);
 
-      expect(price).toHaveTextContent('18$');
+      expect(count).toHaveTextContent('3');
+    });
+
+    test('goes back to add button after multiple changes in the count', () => {
+      const typeTraditional = screen.getByText(/traditional/i);
+      const typeThin = screen.getByText(/thin/i);
+      const twelve = screen.getByText(/12 inch/i);
+      const fourteen = screen.getByText(/14 inch/i);
+      const sixteen = screen.getByText(/16 inch/i);
+      const buttonAdd = screen.getByText(/add/i);
+      userEvent.click(buttonAdd);
+
+      const buttonPlus = screen.getByTestId('itemsHandlerPlus');
+      const buttonMinus = screen.getByTestId('itemsHandlerMinus');
+
+      userEvent.click(buttonPlus);
+
+      userEvent.click(typeThin);
+      userEvent.click(fourteen);
+      userEvent.click(buttonPlus);
+      userEvent.click(buttonPlus);
+
+      userEvent.click(sixteen);
+      userEvent.click(buttonPlus);
+
+      userEvent.click(fourteen);
+      userEvent.click(buttonMinus);
+      userEvent.click(buttonMinus);
+      userEvent.click(buttonMinus);
+
+      userEvent.click(sixteen);
+      userEvent.click(buttonMinus);
+
+      userEvent.click(twelve);
+      userEvent.click(typeTraditional);
+      userEvent.click(buttonMinus);
+      userEvent.click(buttonMinus);
+
+      expect(screen.getByText(/add/i)).toBeInTheDocument();
     });
   });
 });
