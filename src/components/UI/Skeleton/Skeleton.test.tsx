53df859dfded798122f7f64b25f723f38ccfd6b8
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-02-09 23:43:27 -0500

    feat: implement test for Skeleton

---
+++ src/components/UI/Skeleton/Skeleton.test.tsx
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/react';
+
+import Skeleton from './Skeleton';
+import renderWithAllProviders from '../../../tests/helpers/renderWithProviders';
+
+describe('Skeleton Tests', () => {
+  test('renders the Skeleton component', () => {
+    renderWithAllProviders(<Skeleton />);
+    expect(screen.getByRole('presentation')).toBeInTheDocument();
+  });
+});
