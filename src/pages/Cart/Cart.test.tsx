acc256efa957c8645382e7b46243b74bfdfc2fd7
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-02-11 03:34:21 -0500

    feat: add tests for the Cart page

---
+++ src/pages/Cart/Cart.test.tsx
@@ -0,0 +1,48 @@
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import Cart from './Cart';
+import rendererWithProviders from '../../tests/helpers/rendererWithProviders';
+import renderWithProviders from '../../tests/helpers/renderWithProviders';
+import { CartItemsMockProps } from '../../tests/mocks/api/mockData';
+
+describe('Cart Tests', () => {
+  test('renders the Cart page', () => {
+    const snapshot = rendererWithProviders(<Cart />);
+    expect(snapshot).toMatchSnapshot();
+  });
+
+  test('renders the Cart page with items', () => {
+    const snapshot = rendererWithProviders(<Cart />, '/', {
+      cart: CartItemsMockProps,
+    });
+    expect(snapshot).toMatchSnapshot();
+  });
+
+  test('deletes all items from the cart', () => {
+    renderWithProviders(<Cart />, false, '/', {
+      cart: CartItemsMockProps,
+    });
+    const deleteButton = screen.getByText(/delete all items/i);
+
+    userEvent.click(deleteButton);
+
+    expect(screen.getByRole('heading')).toHaveTextContent(
+      /your cart is empty 😕/i,
+    );
+  });
+
+  test('checks redirect to the Home page', () => {
+    renderWithProviders(null, true, '/cart', {
+      cart: CartItemsMockProps,
+    });
+    const link = screen.getByRole('link', {
+      name: /go back/i,
+    });
+
+    userEvent.click(link);
+
+    expect(screen.getByText(/all pizzas/i)).toBeInTheDocument();
+  });
+});
