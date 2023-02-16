45d177c6337e32e804ce30c069bff93f90342910
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-02-16 14:59:03 -0500

    feat: install cypress/testing library plugin

--- cypress/e2e/ItemCardTest.cy.ts
+++ cypress/e2e/ItemCardTest.cy.ts
@@ -1,7 +1,10 @@
 /// <reference types="cypress" />
+import '@testing-library/cypress/add-commands';
 
 describe('ItemCard test', () => {
   it('tests all ItemCard logic', () => {
     cy.visit('/');
+
+    cy.findByRole('heading', { name: /pizza place/i });
   });
 });
---
+++ cypress/support/commands.ts
@@ -0,0 +1,6 @@
+/// <reference types="cypress" />
+import '@testing-library/cypress/add-commands';
+
+Cypress.Commands.add('configureCypressTestingLibrary', (config) => {
+  cy.configureCypressTestingLibrary(config);
+});
