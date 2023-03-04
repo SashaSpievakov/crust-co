ccd0ecfa87590c4455fa1866c493cfbacb17bbc4
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-03-04 02:34:18 -0500

    feat: complete all tests for Modal

--- src/components/UI/Modal/Modal.test.tsx
+++ src/components/UI/Modal/Modal.test.tsx
@@ -13,29 +13,125 @@ describe('Modal Tests', () => {
     expect(snapshot).toMatchSnapshot();
   });
 
-  test('closes Modal by clicking on the cross', async () => {
-    renderWithProviders(null, true, '/cart', {
-      cart: CartItemsMockProps,
+  describe('checks Modal logic', () => {
+    beforeEach(() => {
+      renderWithProviders(null, true, '/cart', {
+        cart: CartItemsMockProps,
+      });
     });
-    const buyButton = screen.getByRole('button', {
-      name: /buy now/i,
+
+    test('checks Modal opening', () => {
+      const buyButton = screen.getByRole('button', {
+        name: /buy now/i,
+      });
+
+      userEvent.click(buyButton);
+
+      expect(
+        screen.getByRole('heading', {
+          name: 'Order',
+        }),
+      ).toBeInTheDocument();
     });
 
-    userEvent.click(buyButton);
+    test('closes Modal by clicking on the cross', () => {
+      const buyButton = screen.getByRole('button', {
+        name: /buy now/i,
+      });
 
-    const cross = screen.getByTestId('modalCross');
-    expect(
-      screen.getByRole('heading', {
-        name: 'Order',
-      }),
-    ).toBeInTheDocument();
+      userEvent.click(buyButton);
 
-    userEvent.click(cross);
+      const cross = screen.getByTestId('modalCross');
 
-    expect(
-      screen.queryByRole('heading', {
-        name: 'Order',
-      }),
-    ).not.toBeInTheDocument();
+      userEvent.click(cross);
+
+      expect(
+        screen.queryByRole('heading', {
+          name: 'Order',
+        }),
+      ).not.toBeInTheDocument();
+    });
+
+    test('checks required fields error', async () => {
+      const buyButton = screen.getByRole('button', {
+        name: /buy now/i,
+      });
+
+      userEvent.click(buyButton);
+
+      const sumbitBtn = screen.getByRole('button', {
+        name: /send/i,
+      });
+
+      userEvent.click(sumbitBtn);
+
+      expect(
+        await screen.findAllByText(/this field is required/i),
+      ).toHaveLength(3);
+    });
+
+    test('checks min characters phone input error', async () => {
+      const buyButton = screen.getByRole('button', {
+        name: /buy now/i,
+      });
+
+      userEvent.click(buyButton);
+
+      const sumbitBtn = screen.getByRole('button', {
+        name: /send/i,
+      });
+      const phoneInput = screen.getByRole('spinbutton');
+
+      userEvent.type(phoneInput, '12353');
+      userEvent.click(sumbitBtn);
+
+      expect(
+        await screen.findByText(/the phone number should have 10 characters/i),
+      ).toBeInTheDocument();
+    });
+
+    test('checks max characters phone input error', async () => {
+      const buyButton = screen.getByRole('button', {
+        name: /buy now/i,
+      });
+
+      userEvent.click(buyButton);
+
+      const sumbitBtn = screen.getByRole('button', {
+        name: /send/i,
+      });
+      const phoneInput = screen.getByRole('spinbutton');
+
+      userEvent.type(phoneInput, '12345678901');
+      userEvent.click(sumbitBtn);
+
+      expect(
+        await screen.findByText(/the phone number should have 10 characters/i),
+      ).toBeInTheDocument();
+    });
+
+    test('successfully sumbits data', async () => {
+      const buyButton = screen.getByRole('button', {
+        name: /buy now/i,
+      });
+
+      userEvent.click(buyButton);
+
+      const sumbitBtn = screen.getByRole('button', {
+        name: /send/i,
+      });
+      const inputs = screen.getAllByRole('textbox');
+      const phoneInput = screen.getByRole('spinbutton');
+
+      userEvent.type(phoneInput, '4730395859');
+      userEvent.type(inputs[0], 'Alex');
+      userEvent.type(inputs[1], 'Toronto');
+      userEvent.type(inputs[2], 'some address');
+      userEvent.click(sumbitBtn);
+
+      expect(
+        await screen.findByText(/thank you for your order!/i),
+      ).toBeInTheDocument();
+    });
   });
 });
