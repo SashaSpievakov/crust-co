298ff13cb07e290aa82b806ed67a935d7d7b0f6f
Sasha <spievakov@gmail.com>
2024-02-27 00:17:54 -0500

    create a separate input UI component

---
+++ src/components/UI/Input/Input.tsx
@@ -0,0 +1,44 @@
+import { forwardRef, HTMLInputTypeAttribute, KeyboardEvent } from 'react';
+import { ChangeHandler } from 'react-hook-form';
+
+import { Group, Label, StyledInput } from './Input.styled';
+
+interface InputProps {
+  id: string;
+  name?: string;
+  type?: HTMLInputTypeAttribute | undefined;
+  onChange?: ChangeHandler | (() => void);
+  onBlur?: ChangeHandler | (() => void);
+  label?: string;
+  error?: undefined | string;
+}
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ id, name, type, onChange, onBlur, label, error }, ref) => {
+    const handlePhoneInputKeyDownClick = (
+      e: KeyboardEvent<HTMLInputElement>,
+    ) => {
+      if (type === 'number' && (e.key === 'e' || e.key === '-')) {
+        e.preventDefault();
+      }
+    };
+
+    return (
+      <Group>
+        {label && <Label htmlFor={id}>{label}</Label>}
+
+        <StyledInput
+          id={id}
+          name={name}
+          type={type}
+          ref={ref}
+          onChange={onChange}
+          onBlur={onBlur}
+          onKeyPress={(e) => handlePhoneInputKeyDownClick(e)}
+        />
+
+        {error && <span>{error}</span>}
+      </Group>
+    );
+  },
+);
--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,5 +1,6 @@
 export { ButtonLink } from './ButtonLink';
 export { DropdownSelect } from './DropdownSelect';
+export { Input } from './Input';
 export { Loading } from './Loading';
 export { SearchItems } from './SearchItems';
 export { SkeletonLoader } from './SkeletonLoader';
